3ef759d8412d89ad380883ee530f64cfe4cad482
Dr Nic Williams <drnicwilliams@gmail.com>
2012-07-02 16:08:43 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubySamlIdpRails3Example::Application.config.secret_token = '584331418f268e2e9629dd3ab5e6d6fee583ed0932930b671cfb68bf95d29811c9404d8f1feb19174dd76175e7b011c531f923e9d55bab5bfdedefacf6aab689'
