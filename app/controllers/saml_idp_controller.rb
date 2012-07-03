991b9a969f364e345341a61841989fd517d63101
Dr Nic Williams <drnicwilliams@gmail.com>
2012-07-03 08:33:22 -0700

    bump to latest ruby-saml-idp; remove explicit uuid loading

--- app/controllers/saml_idp_controller.rb
+++ app/controllers/saml_idp_controller.rb
@@ -1,25 +1,11 @@
-require "uuid"
-
 class SamlIdpController < SamlIdp::IdpController
-  # before_filter :find_account
 
   def idp_authenticate(email, password)
-    # user = @account.users.where(:email => params[:email]).first
-    # user && user.valid_password?(params[:password]) ? user : nil
     true
   end
 
   def idp_make_saml_response(user)
-    # encode_SAMLResponse(user.email)
-    encode_SAMLResponse("drnicwilliams@gmail.com")
+    encode_SAMLResponse("you@example.com")
   end
-
-  private
-
-    def find_account
-      @subdomain = saml_acs_url[/https?:\/\/(.+?)\.example.com/, 1]
-      @account = Account.find_by_subdomain(@subdomain)
-      render :status => :forbidden unless @account.saml_enabled?
-    end
 
 end
